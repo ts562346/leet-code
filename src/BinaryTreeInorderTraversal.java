1cdfc23e037fc80635b580838060b111308675c0
tasneemhoque <tasneemhaque@hotmail.com>
2024-03-28 00:38:32 -0300

    added new problem

--- src/BinaryTreeInorderTraversal.java
+++ src/BinaryTreeInorderTraversal.java
@@ -1,7 +1,31 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
+/*
+
+94. Binary Tree Inorder Traversal - Easy
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+ */
 
 public class BinaryTreeInorderTraversal {
     public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
 
+        while (current != null || !stack.isEmpty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+
+            current = stack.pop();
+            result.add(current.val);
+            current = current.right;
+        }
+
+        return result;
     }
 }

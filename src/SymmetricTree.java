8061409a2ff29bc28e509df94d2e336fb468d3f8
tasneemhoque <tasneemhoque786@gmail.com>
2024-11-24 14:05:19 -0400

    solved problem 101

--- src/SymmetricTree.java
+++ src/SymmetricTree.java
@@ -7,6 +7,18 @@ Given the root of a binary tree, check whether it is a mirror of itself (i.e., s
  */
 public class SymmetricTree {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null) return true;
+        if (root == null) {
+            return true;
+        }
+        return isMirror(root.left, root.right);
+    }
+
+    private boolean isMirror(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        } else if (node1 == null || node2 == null) {
+            return false;
+        }
+        return node1.val == node2.val && isMirror(node1.left, node2.right) && isMirror(node1.right, node2.left);
     }
 }
--- test/SymmetricTreeTest.java
+++ test/SymmetricTreeTest.java
@@ -1,2 +1,54 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SymmetricTreeTest {
+    /*
+        Example 1:
+
+        Input: root = [1,2,2,3,4,4,3]
+        Output: true
+     */
+    @Test
+    public void example1(){
+        TreeNode levelTwoLeft = new TreeNode(2);
+        levelTwoLeft.left = new TreeNode(3);
+        levelTwoLeft.right = new TreeNode(4);
+
+        TreeNode levelTwoRight = new TreeNode(2);
+        levelTwoRight.left = new TreeNode(4);
+        levelTwoRight.right = new TreeNode(3);
+
+        TreeNode root = new TreeNode(1);
+        root.left = levelTwoLeft;
+        root.right = levelTwoRight;
+
+        SymmetricTree tree = new SymmetricTree();
+
+        Assertions.assertTrue(tree.isSymmetric(root));
+    }
+
+    /*
+        Example 2:
+
+        Input: root = [1,2,2,null,3,null,3]
+        Output: false
+     */
+    @Test
+    public void example2(){
+        TreeNode levelTwoLeft = new TreeNode(2);
+        levelTwoLeft.left = null;
+        levelTwoLeft.right = new TreeNode(3);
+
+        TreeNode levelTwoRight = new TreeNode(2);
+        levelTwoRight.left = null;
+        levelTwoRight.right = new TreeNode(3);
+
+        TreeNode root = new TreeNode(1);
+        root.left = levelTwoLeft;
+        root.right = levelTwoRight;
+
+        SymmetricTree tree = new SymmetricTree();
+
+        Assertions.assertFalse(tree.isSymmetric(root));
+    }
 }
